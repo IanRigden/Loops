0d1b827fb7c2c3eeb3293d61f47d359a963dca00
Ian Rigden <ianrigden@hotmail.com>
2022-11-17 16:39:17 +0000

    Initial commit

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,14 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        //Simple do loop 0 to 19
+        int x = 0;
+
+        do
+        {
+            System.out.print("value of x : " + x );
+            x++;
+            System.out.print("\n");
+        } while ( x < 20 );
+
+    }
+}
---
+++ src/WhileLoop.java
@@ -0,0 +1,14 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        //while loop of 0 to 19
+        int x = 0;
+
+        while( x < 20 )
+        {
+            System.out.print("value of x : " + x );
+            x++;
+            System.out.print("\n");
+        }
+
+    }
+}
